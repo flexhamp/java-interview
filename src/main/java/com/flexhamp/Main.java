7bd9d01435e41258ab543c32309f54b2f538081f
Kuzmenko Nikita <flexhamp@gmail.com>
2023-11-23 18:35:42 +0300

    Fix state

---
+++ src/main/java/com/flexhamp/Main.java
@@ -0,0 +1,11 @@
+package com.flexhamp;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        final LinkedList<String> strings = new LinkedList<>();
+        final ArrayList<String> strings1 = new ArrayList<>();
+    }
+}
