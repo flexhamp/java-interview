3df97dc27a2ee111df94c0d43f20ce322e358bbc
Kuzmenko Nikita <flexhamp@gmail.com>
2023-11-25 23:42:58 +0300

    Fix state

---
+++ spring-boot/src/main/java/com/flexhamp/springboot/kafka/producer/config/AppConfig.java
@@ -0,0 +1,42 @@
+package com.flexhamp.springboot.kafka.producer.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flexhamp.springboot.kafka.producer.model.StringValue;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.JacksonUtils;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+public class AppConfig {
+    public final String topicName;
+
+    public AppConfig(String topicName) {
+        this.topicName = topicName;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JacksonUtils.enhancedObjectMapper();
+    }
+
+    @Bean
+    public ProducerFactory<String, StringValue> producerFactory(KafkaProperties kafkaProperties, ObjectMapper mapper) {
+        var props = kafkaProperties.buildProducerProperties();
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        var kafkaProducerFactory = new DefaultKafkaProducerFactory<String, StringValue>(props);
+        kafkaProducerFactory.setValueSerializer(new JsonSerializer<>(mapper));
+        return kafkaProducerFactory;
+    }
+
+    @Bean
+    public KafkaTemplate<String, StringValue> kafkaTemplate(ProducerFactory<String, StringValue> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
---
+++ spring-boot/src/main/java/com/flexhamp/springboot/kafka/producer/model/StringValue.java
@@ -0,0 +1,4 @@
+package com.flexhamp.springboot.kafka.producer.model;
+
+public record StringValue(long id, String value) {
+}
