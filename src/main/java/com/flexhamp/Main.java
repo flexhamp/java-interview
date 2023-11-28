c70b5baf684b380b2e53239e74e0e11da8e9ea6d
Kuzmenko Nikita <flexhamp@gmail.com>
2023-11-28 19:55:14 +0300

    Fix state

--- src/main/java/com/flexhamp/Main.java
+++ src/main/java/com/flexhamp/Main.java
@@ -2,10 +2,18 @@ package com.flexhamp;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         final LinkedList<String> strings = new LinkedList<>();
         final ArrayList<String> strings1 = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+
+        test(list);
+    }
+
+    public static void test(List<? extends Number> list) {
+
     }
 }
