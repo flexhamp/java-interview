e0faecc1e8a5ab61dde211174ee88fdba1daea47
Kuzmenko Nikita <flexhamp@gmail.com>
2023-11-28 20:09:38 +0300

    Fix state

--- src/main/java/com/flexhamp/multithreading/CallableExample.java
+++ src/main/java/com/flexhamp/multithreading/CallableExample.java
@@ -6,9 +6,26 @@ import java.util.concurrent.FutureTask;
 public class CallableExample {
     public static void main(String[] args) throws Exception {
         Callable<Integer> callable = new MyCallable();
-        FutureTask futureTask = new FutureTask(callable);
+        FutureTask<Integer> futureTask = new FutureTask(callable);
         new Thread(futureTask).start();
-        System.out.println(futureTask.get());
+
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println(futureTask.isDone());
+//            System.out.println(i);
+//            Thread.sleep(1000);
+//        }
+
+        int i = 0;
+        while (!futureTask.isDone() || i > 100) {
+            i++;
+            System.out.println(i);
+            if (i > 28)
+                futureTask.cancel(true);
+            Thread.sleep(1000);
+        }
+
+        final Integer result = futureTask.get();
+        System.out.println(result);
     }
 
     static class MyCallable implements Callable<Integer> {
@@ -17,7 +34,7 @@ public class CallableExample {
         public Integer call() throws Exception {
             int j = 0;
             for (int i = 0; i < 10; i++, j++) {
-                Thread.sleep(300);
+                Thread.sleep(3000);
             }
             return j;
         }
