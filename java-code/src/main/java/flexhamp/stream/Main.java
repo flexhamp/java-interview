96cbbe6354ad7d91a232e816baf7e533cede35b6
Kuzmenko Nikita <flexhamp@gmail.com>
2023-11-30 21:32:49 +0300

    Fix state

--- java-code/src/main/java/flexhamp/stream/Main.java
+++ java-code/src/main/java/flexhamp/stream/Main.java
@@ -1,11 +1,41 @@
 package flexhamp.stream;
 
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // Создание стрима
 
+        // От коллекции
+        Collection<Integer> list = new ArrayList<>();
+        Stream<Integer> stream1 = list.stream();
+
+        // Из массива
+        int[] numbers = {1, 2, 3};
+        final IntStream stream5 = Arrays.stream(numbers);
+        Stream<Integer> stream2 = stream5.boxed();
+
+        // Из файла
+        Path path = Paths.get("file.txt");
+        try (Stream<String> stream3 = Files.lines(path)) {
+
+        }
+
+        // Генерирование
+        Stream<Integer> stream4 = Stream.generate(() -> new Random().nextInt());
+
+        // Билдер
+        Stream.Builder<Integer> builder = Stream.builder();
+        builder.add(1);
+        builder.add(2);
+        builder.add(3);
+        Stream<Integer> stream = builder.build();
     }
 
 
