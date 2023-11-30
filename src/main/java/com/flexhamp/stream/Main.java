e8e22e699e14a452e801ce77682b6b39d3a95adf
Kuzmenko Nikita <flexhamp@gmail.com>
2023-11-30 15:51:07 +0300

    FIx state

---
+++ src/main/java/com/flexhamp/stream/Main.java
@@ -0,0 +1,18 @@
+package com.flexhamp.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+    private static <T> String listToString(List<T> list) {
+        if (list == null)
+            return null;
+        return list.stream().map(Object::toString)
+                .collect(Collectors.joining(", "));
+    }
+}
