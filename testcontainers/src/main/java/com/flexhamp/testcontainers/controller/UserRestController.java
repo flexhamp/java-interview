36a7b363079d282f42b486009afa8f28e685f4ad
Kuzmenko Nikita <flexhamp@gmail.com>
2023-12-04 07:44:34 +0300

    Added Testcontainers

---
+++ testcontainers/src/main/java/com/flexhamp/testcontainers/controller/UserRestController.java
@@ -0,0 +1,34 @@
+package com.flexhamp.testcontainers.controller;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserRestController implements RowMapper<UserRestController.User> {
+   private final NamedParameterJdbcOperations jdbcOperations;
+
+    public UserRestController(NamedParameterJdbcOperations jdbcOperations) {
+        this.jdbcOperations = jdbcOperations;
+    }
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new User(rs.getLong("id"), rs.getString("username"));
+    }
+
+    record User(long id, String username) {
+    }
+
+    @GetMapping
+    public List<User> findAllUsers() {
+        return this.jdbcOperations.query("select * from t_user", this);
+    }
+}
