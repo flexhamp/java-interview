e37cd4b3b715c521991ff6d16a1230a20afe5c11
Kuzmenko Nikita <flexhamp@gmail.com>
2023-12-08 13:42:43 +0300

    Fix state

--- testcontainers/src/test/java/com/flexhamp/testcontainers/controller/UserRestControllerTest.java
+++ testcontainers/src/test/java/com/flexhamp/testcontainers/controller/UserRestControllerTest.java
@@ -1,36 +1,31 @@
 package com.flexhamp.testcontainers.controller;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserRestControllerTest {
-//    @Container
-//    @ServiceConnection
-//    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15"));
-
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    Environment environment;
+
     @Test
     void findAllUsers_ReturnsUserList() throws Exception {
+        assertEquals("org.postgresql.Driver", environment.getProperty("postgresql.driver"));
+
         this.mockMvc.perform(get("/api/user/list")).andExpectAll(
                 status().isOk(),
                 content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
