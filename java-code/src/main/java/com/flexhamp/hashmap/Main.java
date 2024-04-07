b6b8e80da932035820d5afd1774a811f3d2d5dff
Kuzmenko Nikita <flexhamp@gmail.com>
2024-04-07 19:04:56 +0300

    Fix state

--- java-code/src/main/java/com/flexhamp/hashmap/Main.java
+++ java-code/src/main/java/com/flexhamp/hashmap/Main.java
@@ -6,20 +6,28 @@ import org.openjdk.jol.vm.VM;
 
 import java.util.HashMap;public class Main {
     public static void main(String[] args) {
-        System.out.println(VM.current().details());
+        System.out.println(tableSizeFor(7));
+//        System.out.println(VM.current().details());
+//
+//        final HashMap<String,String> stringStringHashMap = new HashMap<>(10);
+//
+//        System.out.println(ClassLayout.parseInstance(stringStringHashMap).toPrintable());
+//        System.out.println(GraphLayout.parseInstance(stringStringHashMap).toFootprint());
+//
+//
+//        stringStringHashMap.put("", "");
+//        stringStringHashMap.put("1", "");
+//        stringStringHashMap.put("2", "");
+//        System.out.println("tttt");
+//
+//        System.out.println(ClassLayout.parseInstance(stringStringHashMap).toPrintable());
+//        System.out.println(GraphLayout.parseInstance(stringStringHashMap).toFootprint());
+    }
 
-        final HashMap<String,String> stringStringHashMap = new HashMap<>();
-
-        System.out.println(ClassLayout.parseInstance(stringStringHashMap).toPrintable());
-        System.out.println(GraphLayout.parseInstance(stringStringHashMap).toFootprint());
-
-
-        stringStringHashMap.put("", "");
-        stringStringHashMap.put("1", "");
-        stringStringHashMap.put("2", "");
-        System.out.println("tttt");
-
-        System.out.println(ClassLayout.parseInstance(stringStringHashMap).toPrintable());
-        System.out.println(GraphLayout.parseInstance(stringStringHashMap).toFootprint());
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+    static final int tableSizeFor(int cap) {
+        int n = -1 >>> Integer.numberOfLeadingZeros(cap - 1);
+        System.out.println(n);
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
     }
 }
