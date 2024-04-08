ab93b203424f242ec2978152ffbfe4829eec463a
Kuzmenko Nikita <flexhamp@gmail.com>
2024-04-08 07:37:51 +0300

    Fix state

--- java-code/src/main/java/com/flexhamp/hashmap/Main.java
+++ java-code/src/main/java/com/flexhamp/hashmap/Main.java
@@ -6,19 +6,24 @@ import org.openjdk.jol.vm.VM;
 
 import java.util.HashMap;public class Main {
     public static void main(String[] args) {
-        System.out.println(tableSizeFor(7));
 //        System.out.println(VM.current().details());
-//
-//        final HashMap<String,String> stringStringHashMap = new HashMap<>(10);
-//
-//        System.out.println(ClassLayout.parseInstance(stringStringHashMap).toPrintable());
-//        System.out.println(GraphLayout.parseInstance(stringStringHashMap).toFootprint());
+
+        final HashMap<String,String> map = new HashMap<>(64, 0.75F);
+        System.out.println(ClassLayout.parseInstance(map).toPrintable());
+
+        for (int i = 0; i < 25; i++) {
+            map.put(String.valueOf(i),String.valueOf(i));
+            System.out.println(ClassLayout.parseInstance(map).toPrintable());
+            System.out.println();
+        }
+
+        System.out.println(GraphLayout.parseInstance(map).toFootprint());
 //
 //
 //        stringStringHashMap.put("", "");
 //        stringStringHashMap.put("1", "");
 //        stringStringHashMap.put("2", "");
-//        System.out.println("tttt");
+        System.out.println("tttt");
 //
 //        System.out.println(ClassLayout.parseInstance(stringStringHashMap).toPrintable());
 //        System.out.println(GraphLayout.parseInstance(stringStringHashMap).toFootprint());
@@ -27,7 +32,11 @@ import java.util.HashMap;public class Main {
     static final int MAXIMUM_CAPACITY = 1 << 30;
     static final int tableSizeFor(int cap) {
         int n = -1 >>> Integer.numberOfLeadingZeros(cap - 1);
-        System.out.println(n);
         return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    static final int hash(int hashCode) {
+        int h;
+        return (h = hashCode) ^ (h >>> 16);
     }
 }
